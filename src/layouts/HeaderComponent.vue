7724d1edf91f6d61a300808aa481e9b1175874f6
dmg04144 <dmg04144@naver.com>
2024-02-06 17:58:27 +0900

    02/06

--- src/layouts/HeaderComponent.vue
+++ src/layouts/HeaderComponent.vue
@@ -1,13 +1,13 @@
 <template>
     <ul class="nav justify-content-center">
         <li class="nav-item">
-            <router-link class="nav-link active" aria-current="page" to="#">전체조회</router-link>
+            <router-link class="nav-link active" aria-current="page" to="/">전체조회</router-link>
         </li>
         <li class="nav-item">
-            <router-link class="nav-link" to="#">등록</router-link>
+            <router-link class="nav-link" to="/userInsert">등록</router-link>
         </li>
         <li class="nav-item">
-            <router-link class="nav-link disabled" aria-disabled="true" to="#">Disabled</router-link>
+            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
         </li>
     </ul>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,30 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import UserListView from '../views/UserListView.vue'
+import UserInfoView from '../views/UserInfoView.vue'
+import UserInsertView from '../views/UserInsertView.vue'
+import UserUpdateView from '../views/UserUpdateView.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'UserListView',
+    name: 'userList',
     component: UserListView
+  },
+  {
+    path: '/userInfo',
+    name: 'userInfo',
+    component: UserInfoView
+  }
+  ,
+  {
+    path: '/userInsert',
+    name: 'userInsert',
+    component: UserInsertView
+  },
+  {
+    path: '/userUpdate',
+    name: 'userUpdate',
+    component: UserUpdateView
   }
 ]
 
